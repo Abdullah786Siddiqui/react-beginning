69c0f36df87f6b92831905d3c814e7319a46fc3d
Abdullah Siddiqui <abdullahsidzz333@gmail.com>
2024-12-24 13:21:42 +0500

    push

---
+++ ATM-APP/1/src/App.jsx
@@ -0,0 +1,6 @@
+import Input from "./Components/input"
+function App() {
+  <Input />
+}
+
+export default App;
---
+++ ATM-APP/1/src/Components/input.jsx
@@ -0,0 +1,11 @@
+let Input = () => {
+  return (
+    <div className="container">
+      <h1>REACT ATM APP</h1>
+      <label htmlFor="passwordKey">
+        <input type="number" id="passwordKey" />
+      </label>
+    </div>
+  );
+};
+export default Input;
